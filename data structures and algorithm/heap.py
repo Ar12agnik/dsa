81b9cf165df5dc7b14502573c6edc0e25b36fc93
Ar12agnik <102238499+Ar12agnik@users.noreply.github.com>
2023-05-13 00:33:41 +0530

    final update

--- data structures and algorithm/heap.py
+++ data structures and algorithm/heap.py
@@ -1,1 +1,10 @@
-#to be continued!!
+from heapq import heappush,heappop
+def heapsort():
+    global list1
+    heap=[]
+    for ele in list1:
+        heappush(heap,ele)
+    sort=[]
+    while heap:
+        sort.append(heappop(heap))
+    return sort
