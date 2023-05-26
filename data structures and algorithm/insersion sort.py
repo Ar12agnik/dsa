bc466bd8be28bf03617ce72ec9e6db717f8bca48
Ar12agnik <102238499+Ar12agnik@users.noreply.github.com>
2023-05-26 12:17:18 +0530

    Create insersion sort.py

---
+++ data structures and algorithm/insersion sort.py
@@ -0,0 +1,7 @@
+x=[19,18,17,16,15,14,13,12,11,4]
+for i in range(0,len(x)):
+    for j in range(i,-1,-1):
+        if j-1>-1:
+            if x[j-1]>x[j]:
+                x[j],x[j-1]=x[j-1],x[j]
+print(x)
