3db242ff9de9ca94946bdf73c50ed04a4b025f52
Agnik Roy <102238499+Ar12agnik@users.noreply.github.com>
2023-06-18 18:06:41 +0530

    Update insersion sort.py

--- data structures and algorithm/insersion sort.py
+++ data structures and algorithm/insersion sort.py
@@ -1,3 +1,5 @@
+#each element asks the previous element ami ki tormar #theke choto?
+#first part sorted 19 sorted 
 x=[19,18,17,16,15,14,13,12,11,4]
 for i in range(0,len(x)):
     for j in range(i,-1,-1):
